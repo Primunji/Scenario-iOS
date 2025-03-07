059dcac472b6e45606fcebdbf786ea23fa043545
rkdduss <kangyeon0708@naver.com>
2025-03-08 05:12:34 +0900

    feat contact

---
+++ Scenario-iOS/Sourses/Components/ContactButton.swift
@@ -0,0 +1,18 @@
+//
+//  ContactButton.swift
+//  Scenario-iOS
+//
+//  Created by dgsw07 on 3/8/25.
+//
+
+import SwiftUI
+
+struct ContactButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContactButton()
+}
