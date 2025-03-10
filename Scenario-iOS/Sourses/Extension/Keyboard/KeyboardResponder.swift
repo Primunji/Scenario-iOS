0e7d025832769307b6473f85326d17345af5c30a
rkdduss <kangyeon0708@naver.com>
2025-03-10 12:17:05 +0900

    add keyboardresponder

--- Scenario-iOS/Sourses/Extension/Keyboard/KeyboardResponder.swift
+++ Scenario-iOS/Sourses/Extension/Keyboard/KeyboardResponder.swift
@@ -5,4 +5,24 @@
 //  Created by dgsw07 on 3/10/25.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+class KeyboardResponder: ObservableObject {
+    @Published var currentHeight: CGFloat = 0
+    
+    private var cancellable: AnyCancellable?
+    
+    init() {
+        cancellable = Publishers.Merge(
+            NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)
+                .map { notification -> CGFloat in
+                    let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect
+                    return keyboardFrame?.height ?? 0
+                },
+            NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
+                .map { _ in CGFloat(0) }
+        )
+        .assign(to: \.currentHeight, on: self)
+    }
+}
